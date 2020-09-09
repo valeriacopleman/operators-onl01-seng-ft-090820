e1fee063676ee2f732150de81784c9fa59f2c829
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-09 17:22:52 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,15 @@
 def unsafe?(speed)
   if speed > 40 && speed < 60
-    return TRUE
+    return "TRUE"
   else
-    return FALSE
+    return "FALSE"
 
 end
 
 
 
 def not_safe?(speed)
-  speed > 40 && speed < 60 ? true : FALSE
+  speed > 40 && speed < 60 ? "true" : "FALSE"
 	
 end
 	
